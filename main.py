ce95a8416e6314c8ff1e21138dda49132ce3fba6
Aymane Boukrouh <boukrouhaymane@gmail.com>
2022-03-24 23:45:21 +0100

    add say_hello

--- main.py
+++ main.py
@@ -3,6 +3,9 @@
 def f(x: int) -> int:
     '''return double of an integer'''
     return 2*x
-  
+
+def say_hello(name: str):
+    print(f'Hello {name}!')
+
 if __name__ == '__main__':
     print(f(0))
